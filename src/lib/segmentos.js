f01275a76817edbf9f6c32a57d2e13ebe42ffbba
Pedro Gonzalez Soro <pedrogonzalezsoro@192.168.1.16>
2024-11-08 18:27:46 -0300

    mapeo todos las marcas

--- src/lib/segmentos.js
+++ src/lib/segmentos.js
@@ -24,7 +24,6 @@ try {
       nombre: item.nombre.toUpperCase(), // Convertir el nombre a mayúsculas
     };
   });
-  console.log("Segmentos obtenidos:", segmentos);
 } catch (error) {
   console.error("Error al autenticarse o al obtener el registro:", error);
 }
