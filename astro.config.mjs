7e5680e23a62125be9ec5471a3f7197dbf289bbe
dan-dervich <dandervich@hotmail.com>
2024-11-14 13:43:51 -0300

    agregar prefetch a los links de afw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react()],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true,
+  },
 });
