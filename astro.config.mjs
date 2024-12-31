15917358c819ac90153fd457424b28d6643d13e5
dan-dervich <dandervich@hotmail.com>
2024-12-31 17:49:53 -0300

    removed vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
